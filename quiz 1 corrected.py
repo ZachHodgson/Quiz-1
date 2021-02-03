3d37a2a506cb9f28023571fb195d752eca5a8552
ZachHodgson <77622287+ZachHodgson@users.noreply.github.com>
2021-02-03 10:32:46 -0400

    Add files via upload

---
+++ quiz 1 corrected.py
@@ -0,0 +1,19 @@
+import math
+from time import sleep
+
+
+def function():
+    f0 = 0
+    f1 = 1
+    inp = 'c'
+    while inp != 'q':
+        sfn = f1 + f0
+        print(" sFn = %d" % sfn)
+        f0 = f1
+        f1 = sfn
+        print("Press c to continue or q to quit")
+        inp = input()
+
+
+if __name__ == '__main__':
+    function()
